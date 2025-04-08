96a80a593240a1cf6d80b38e48f4a66e201570e3
Maximiliano De Pietro <elwindwalker@gmail.com>
2025-04-08 20:43:37 -0300

    Agrego texto de prueba en Greeting

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/screens/UserScreen.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/screens/UserScreen.kt
@@ -1,9 +1,11 @@
 package ar.edu.unlam.mobile.scaffolding.ui.screens
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import ar.edu.unlam.mobile.scaffolding.ui.components.Greeting
 
 @Composable
 fun UserScreen(userId: String) {
     Greeting(userId)
+    Text("Texto de prueba")
 }
