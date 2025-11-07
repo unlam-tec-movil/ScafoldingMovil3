d19720d702526e0202285819652ffb3205d08df3
Kiara <kiara.lopez11106@gmail.com>
2025-11-07 20:11:10 -0300

    a

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,5 +8,4 @@ plugins {
     alias(libs.plugins.ktlint) apply false
     // Add the dependency for the Google services Gradle plugin
     id("com.google.gms.google-services") version "4.4.4" apply false
-
 }
