05363ecd3b46ebeed44a69520a43fff323e48ab5
cacamanRPG <cacamanmierdaman6@gmail.com>
2025-11-09 22:54:07 -0300

    Ktlint format hecho

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/screens/map/MapViewModel.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/screens/map/MapViewModel.kt
@@ -19,16 +19,15 @@ class MapViewModel
         private val locationRepository: LocationRepository,
         private val pinRepository: PinRepository,
     ) : ViewModel() {
+        private val _pets = MutableStateFlow<List<Pin>>(emptyList())
+        val pets: StateFlow<List<Pin>> = _pets
 
-    private val _pets = MutableStateFlow<List<Pin>>(emptyList())
-    val pets: StateFlow<List<Pin>> = _pets
-
-    init {
-        viewModelScope.launch {
-           // _pets.value =
-            //TODO: Obtener todas las masacotas
+        init {
+            viewModelScope.launch {
+                // _pets.value =
+                // TODO: Obtener todas las masacotas
+            }
         }
-    }
         // ===== ESTADO DE UBICACIÓN =====
 
         /**
@@ -143,7 +142,7 @@ class MapViewModel
                         // Lógica de generación de ID
                         latitude = latitude,
                         longitude = longitude,
-                        imageUrl ="https://cdn.pixabay.com/photo/2023/04/28/12/18/dogs-7956516_640.jpg",
+                        imageUrl = "https://cdn.pixabay.com/photo/2023/04/28/12/18/dogs-7956516_640.jpg",
                     )
 
                 val result = pinRepository.savePin(newPin)
